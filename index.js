77eec3799067d203258f235303c9e272f8ec719d
Adam Seid Tahir <adam.seidtahir@hotmail.com>
2019-01-14 12:17:24 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
+
+var express = require('express');
+var app = express();
+var path = require('path');
 express.static(root, [options])
 app.use(express.static('public'))
+
+// viewed at http://localhost:8080
+app.get('/', function(req, res) {
+    res.sendFile(path.join(__dirname + '/index.htm'));
+});
+
+app.listen(8080);

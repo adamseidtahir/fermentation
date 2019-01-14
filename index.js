2527c553aedf25347a84cca228ff7fd18420bcb8
Adam Seid Tahir <adam.seidtahir@hotmail.com>
2019-01-14 12:20:48 +0100

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 var express = require('express');
 var app = express();
 var path = require('path');
-express.static(root, [options])
+
 app.use(express.static('public'))
 
 // viewed at http://localhost:8080
